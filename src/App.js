8f553f81cfbb91e0662bab7c22f2b9c9a3291152
Siraj Kakeh <skakeh@winfooz.com>
2019-11-22 04:28:54 +0200

    version 2.0.0 ready

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useRef } from 'react';
+import Barcode from 'react-barcode';
 import './App.css';
+import winfooz128 from './images/winfooz128.png';
+import spinner from './images/spinner.svg';
+
+const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [vin, setVin] = useState('');
+  const inputRef = useRef(null);
+
+  const copyVin = () => {
+    inputRef.current.select();
+    document.execCommand('copy');
+  };
+
+  const getVin = (type = 'fake') => {
+    if (isLoading) return;
+    setIsLoading(true);
+    fetch(`${proxyUrl}http://randomvin.com/getvin.php?type=${type}`)
+      .then(res => res.text())
+      .then(vin => {
+        setVin(vin);
+        setIsLoading(false);
+        copyVin();
+      })
+      .catch(console.error);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="background-pics">
+        <img src={winfooz128} alt="logo-neutral" />
+        <img src={winfooz128} alt="logo-neutral" />
+        <img src={winfooz128} alt="logo-neutral" />
+        <img src={winfooz128} alt="logo-neutral" />
+      </div>
+      <div className="container-inner">
+        <button className="action-button" onClick={() => getVin()}>
+          Generate & Copy Vin
+        </button>
+        <div className="controls-wrapper">
+          <input value={vin} ref={inputRef} readOnly />
+          <div className={`vin-value ${vin ? 'with-vin' : ''}`}>{vin}</div>
+        </div>
+        {isLoading && <img src={spinner} alt="loading" className="spinner" />}
+        {vin && <Barcode value={vin} displayValue={false} height={75} />}
+        <span className="footer">
+          Developed by &nbsp;
+          <a
+            href="https://www.linkedin.com/in/siraj-kakeh"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Siraj Kakeh
+          </a>
+          &nbsp; for Winfooz<span>&copy;</span>
+        </span>
+      </div>
     </div>
   );
 }
